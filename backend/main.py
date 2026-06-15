3d7e01b5fa4d0b5944ba600a2fe530b2b523e313
Ayushii-Sharma20 <ayushisharma8060@gmail.com>
2026-06-15 15:57:43 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://document-intelligence-81zr.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
